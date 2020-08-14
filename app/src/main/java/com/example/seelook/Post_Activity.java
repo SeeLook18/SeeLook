1135532d213225c83c4a6b406b9197ea24334513
minju9809 <minju9809@naver.com>
2020-08-14 17:49:37 +0900

    동영상 에뮬레이터에 올림..

--- app/src/main/java/com/example/seelook/Post_Activity.java
+++ app/src/main/java/com/example/seelook/Post_Activity.java
@@ -1,14 +1,14 @@
 package com.example.seelook;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Post_Activity extends AppCompatActivity {
 
@@ -38,7 +38,19 @@ public class Post_Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button add_btn = (Button)findViewById(R.id.add_button);
+        face_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("video/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivity(intent);
+            }
+        });
     }
+
 
     public void done_clicked(View v) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
