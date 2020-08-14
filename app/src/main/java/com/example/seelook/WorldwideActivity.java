fd35d05dc7578eb60f13b8ab933fc8f08719d20c
minju9809 <minju9809@naver.com>
2020-08-14 16:35:27 +0900

    레이아웃 다했엉

--- app/src/main/java/com/example/seelook/WorldwideActivity.java
+++ app/src/main/java/com/example/seelook/WorldwideActivity.java
@@ -1,11 +1,11 @@
 package com.example.seelook;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class WorldwideActivity extends AppCompatActivity {
 
@@ -14,12 +14,12 @@ public class WorldwideActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_worldwide);
 
-        Button Back_button=findViewById(R.id.back);
-        Back_button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                finish();
-            }
-        });
+       // Button Back_button=findViewById(R.id.back);
+       // Back_button.setOnClickListener(new View.OnClickListener(){
+        //    public void onClick(View view){
+        //        finish();
+        //    }
+      //  });
 
         Button homeBt = (Button)findViewById(R.id.home_btn);
         homeBt.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/seelook/profile_Activity.java
+++ app/src/main/java/com/example/seelook/profile_Activity.java
@@ -1,17 +1,11 @@
 package com.example.seelook;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.MediaController;
-import android.widget.VideoView;
 
-import java.net.URL;
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class profile_Activity extends AppCompatActivity {
@@ -21,12 +15,12 @@ public class profile_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_);
 
-        Button Back_button=findViewById(R.id.back);
-        Back_button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                finish();
-            }
-        });
+        //Button Back_button=findViewById(R.id.back);
+        //Back_button.setOnClickListener(new View.OnClickListener(){
+        //    public void onClick(View view){
+         //       finish();
+         //   }
+        //});
 
         Button homeBt = (Button)findViewById(R.id.home_btn);
         homeBt.setOnClickListener(new View.OnClickListener() {
