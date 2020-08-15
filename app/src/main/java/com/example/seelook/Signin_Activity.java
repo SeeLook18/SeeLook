5b82b11291f9ef77aaf965ff3d6d7081d5cd13e6
daeun <sung980524@naver.com>
2020-08-16 01:24:17 +0900

    회원가입 개선 -> fire base Authentication에서 확인

--- app/src/main/java/com/example/seelook/Signin_Activity.java
+++ app/src/main/java/com/example/seelook/Signin_Activity.java
@@ -1,7 +1,7 @@
 package com.example.seelook;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,58 +24,77 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 import java.util.HashMap;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Signin_Activity extends AppCompatActivity {
 
     private static final String TAG="Signin_Activity";
-    EditText et_user_name, et_user_email, et_user_password,et_user_password_check;
-    Button btn_register;
 
     private FirebaseAuth firebaseAuth;
-    private DatabaseReference mDatabase;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText et_user_name, et_user_email, et_user_password,et_user_password_check;
+    private Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin_);
 
-        et_user_name = findViewById(R.id.name);
-        et_user_email = findViewById(R.id.email);
-        et_user_password = findViewById(R.id.password);
-        et_user_password_check = findViewById(R.id.password_check);
-        btn_register = findViewById(R.id.register);
-
         //firebase 정의
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        firebaseAuth=FirebaseAuth.getInstance();
 
-        readUser();
+        //레이아웃이랑 연결
+        et_user_name =(EditText) findViewById(R.id.name);
+        et_user_email = (EditText) findViewById(R.id.email);
+        et_user_password = (EditText) findViewById(R.id.password);
+        et_user_password_check = (EditText) findViewById(R.id.password_check);
+        btn_register = (Button)findViewById(R.id.register);
 
+        //회원가입 버튼
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //문자열로
                 final String getUserName = et_user_name.getText().toString();
-                String getUserEmail = et_user_email.getText().toString();
+                final String getUserEmail = et_user_email.getText().toString();
                 final String getUserPassword = et_user_password.getText().toString();
-                String getUserPassword_check=et_user_password_check.getText().toString();
+                final String getUserPassword_check=et_user_password_check.getText().toString();
 
-                if(getUserPassword.equals(getUserPassword_check)){
-                    Log.d(TAG,"비밀 번호 동일"+getUserName+"/"+getUserEmail+"/"+getUserPassword);
-                    final ProgressDialog Dialog=new ProgressDialog(Signin_Activity.this);
-                    //hashmap 만들기
-                    HashMap result = new HashMap<>();
-
-                    result.put("name", getUserName);
-                    result.put("email", getUserEmail);
-                    result.put("password", getUserPassword);
-
-                    writeNewUser("1",getUserName,getUserEmail,getUserPassword);
-                }else{
-                    Toast.makeText(Signin_Activity.this,"비밀 번호가 틀렸습니다. 다시 입력해 주세요",Toast.LENGTH_SHORT).show();
-                    return;
+                //이메일, 이름, 비밀번호가 공백이 아닌 경우
+                if(!getUserEmail.equals("")&&!getUserPassword.equals("")&&!getUserName.equals("")){
+                    //동일한 비밀번호 체크
+                    if(getUserPassword.equals(getUserPassword_check)){
+                        Log.d(TAG,"비밀 번호 동일/"+getUserName+"/"+getUserEmail+"/"+getUserPassword);
+                        createUser(getUserEmail,getUserPassword);
+                    }
+                    else{
+                        Toast.makeText(Signin_Activity.this,"비밀 번호가 틀렸습니다. 다시 입력해 주세요",Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+                //공백인 경우
+                else{
+                    Toast.makeText(Signin_Activity.this,"계정과 비밀번호를 입력하세요",Toast.LENGTH_LONG).show();
                 }
             }
         });
+
+        mAuthListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user= firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG,"onAuthStateChanged:signed in: "+user.getUid());
+                }else{
+                    Log.d(TAG,"onAuthStateChanged:signed out");
+                }
+            }
+        };
 
         Button back=findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener(){
@@ -103,47 +122,56 @@ public class Signin_Activity extends AppCompatActivity {
             }
         });
     }
+    private void createUser(String email,String password){
+        if(!isValidEmail(email)){
+            Log.e(TAG,"createAccount: email이 유효하지 않음");
+            Toast.makeText(Signin_Activity.this,"Email이 유효하지 않습니다",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(isValidPassword(password)){
+            Log.e(TAG,"createAccount: password가 유효하지 않음");
+            Toast.makeText(Signin_Activity.this,"password가 유효하지 않습니다",Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-    private void writeNewUser(String userId, String name, String email, String password) {
-        User user = new User(name, email, password);
-
-        mDatabase.child("users").push().child(userId).setValue(user)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        // Write was successful!
-                        Toast.makeText(Signin_Activity.this, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // Write failed
-                        Toast.makeText(Signin_Activity.this, "저장을 실패했습니다.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-    }
-
-    private void readUser(){
-        mDatabase.child("users").child("1").addValueEventListener(new ValueEventListener() {
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                // Get Post object and use the values to update the UI
-                if(dataSnapshot.getValue(User.class) != null){
-                    User post = dataSnapshot.getValue(User.class);
-                    Log.w("FireBaseData", "getData" + post.toString());
-                } else {
-                    Toast.makeText(Signin_Activity.this, "데이터 없음...", Toast.LENGTH_SHORT).show();
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+                if (!task.isSuccessful()) {//가입 실패시 시
+                    Toast.makeText(Signin_Activity.this,"인증 실패",Toast.LENGTH_SHORT).show();
                 }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                // Getting Post failed, log a message
-                Log.w("FireBaseData", "loadPost:onCancelled", databaseError.toException());
+                else if(task.isSuccessful()){//가입 성공
+                    Toast.makeText(Signin_Activity.this, "가입 성공", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
+    public void onStart(){
+        super.onStart();
+        firebaseAuth.addAuthStateListener(mAuthListener);
+    }
+    public void onStop(){
+        super.onStop();
+        if (mAuthListener != null) {
+            firebaseAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    private boolean isValidPassword(String target){
+        Pattern p = Pattern.compile("(^.*(?=.{6,100})(?=.*[0-9])(?=.*[a-zA-Z]).*$)");
 
+        Matcher m = p.matcher(target);
+        if (m.find() && !target.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    private boolean isValidEmail(String target) {
+        if (target == null || TextUtils.isEmpty(target)){
+            return false;
+        }else{
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
+    }
 }
