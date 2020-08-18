c0a36c442c2888ce7134c4fd96df46bbb8f5816e
minju9809 <minju9809@naver.com>
2020-08-18 18:32:06 +0900

    wpqkf 성공 333

--- app/src/main/java/com/example/seelook/Login_Activity.java
+++ app/src/main/java/com/example/seelook/Login_Activity.java
@@ -4,12 +4,16 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,19 +32,27 @@ public class Login_Activity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthListener;
 
     private EditText et_user_email, et_user_password;
+    private String getUserEmail;
+    private String getUserPassword;
     private Button btn_login;
-
+    private CheckBox auto_login;//체크 박
+    private boolean saveLoginData;//로그인 저장
+    private SharedPreferences appData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
 
+        appData = getSharedPreferences("appData",MODE_PRIVATE);
+        load();
+
         mAuth=FirebaseAuth.getInstance();
         //레이아웃이랑 연
         et_user_email=(EditText)findViewById(R.id.user_id);
         et_user_password=(EditText)findViewById(R.id.user_password);
         btn_login=(Button)findViewById(R.id.login_btn);
+        auto_login=(CheckBox)findViewById(R.id.auto_login);
 
         mAuthListener=new FirebaseAuth.AuthStateListener() {
             @Override
@@ -72,15 +84,24 @@ public class Login_Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        if(saveLoginData){
+            et_user_email.setText(getUserEmail);
+            et_user_password.setText(getUserPassword);
+            auto_login.setChecked(saveLoginData);
+        }
+
         //로그인 버튼
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String getUserEmail = et_user_email.getText().toString();
-                final String getUserPassword = et_user_password.getText().toString();
+
+                getUserEmail = et_user_email.getText().toString();
+                getUserPassword = et_user_password.getText().toString();
 
                 //이메일, 비밀번호가 공백이 아닌 경우
                 if(!getUserEmail.equals("")&&!getUserPassword.equals("")){
+
                     UserLogin(getUserEmail,getUserPassword);
                 }
                 //공백인 경우
@@ -101,6 +122,7 @@ public class Login_Activity extends AppCompatActivity {
                 }
                 else if(task.isSuccessful()){
                     Toast.makeText(Login_Activity.this,"로그인 성공",Toast.LENGTH_SHORT).show();
+                    save();
                     Intent intent = new Intent(
                             getApplicationContext(),
                             Home_Activity1.class);
@@ -118,5 +140,19 @@ public class Login_Activity extends AppCompatActivity {
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
+    }
+    private  void save(){
+        SharedPreferences.Editor editor = appData.edit();
+        editor.putBoolean("SAVE_LOGIN_DATA",auto_login.isChecked());
+        editor.putString("email",getUserEmail);
+        editor.putString("pw",getUserPassword);
+        editor.apply();
+    }
+    private void load(){
+        //SharedPreference 객체.get타입(저장된 이름, 기본 값)
+        //저장된 이름이 존재하지 않을 시 기본 값
+        saveLoginData = appData.getBoolean("SAVE_LOGIN_DATA",false);
+        getUserEmail=appData.getString("email","");
+        getUserPassword=appData.getString("pw","");
     }
 }
