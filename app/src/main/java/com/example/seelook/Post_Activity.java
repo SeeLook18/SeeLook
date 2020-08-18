2d472236e4c1f64284c1133673437bbe9690a179
minju9809 <minju9809@naver.com>
2020-08-18 15:11:26 +0900

    +에러메시지

--- app/src/main/java/com/example/seelook/Post_Activity.java
+++ app/src/main/java/com/example/seelook/Post_Activity.java
@@ -2,7 +2,11 @@ package com.example.seelook;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -10,7 +14,12 @@ import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.net.URL;
+
 public class Post_Activity extends AppCompatActivity {
+
+    private final int SELECT_IMAGE = 1;
+    private final int SELECT_MOVIE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +52,120 @@ public class Post_Activity extends AppCompatActivity {
         add_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setType("video/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivity(intent);
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+
+                i.setType("video/*");
+
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                try {
+
+                    startActivityForResult(i, SELECT_MOVIE);
+
+                } catch (android.content.ActivityNotFoundException e) {
+
+                    e.printStackTrace();
+
+                }
             }
+
         });
+    }
+
+    @Override
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        super.onActivityResult(requestCode, resultCode, intent);
+
+
+
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == SELECT_IMAGE) {
+
+                Uri uri = intent.getData();
+
+                String path = getPath(uri);
+
+                String name = getName(uri);
+
+                String uriId = getUriId(uri);
+
+                Log.e("###",
+
+                        "실제경로 : " + path + "\n파일명 : " + name + "\nuri : " + uri.toString() + "\nuri id : " + uriId);
+
+            } else if (requestCode == SELECT_MOVIE) {
+
+                Uri uri = intent.getData();
+
+                String path = getPath(uri);
+
+                String name = getName(uri);
+
+                String uriId = getUriId(uri);
+
+                Log.e("###",
+
+                        "실제경로 : " + path + "\n파일명 : " + name + "\nuri : " + uri.toString() + "\nuri id : " + uriId);
+
+            }
+
+        }
+
+    }
+
+    // 실제 경로 찾기
+
+    private String getPath(Uri uri) {
+
+        String[] projection = { MediaStore.Images.Media.DATA };
+
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+        cursor.moveToFirst();
+
+        return cursor.getString(column_index);
+
+    }
+
+
+
+    // 파일명 찾기
+
+    private String getName(Uri uri) {
+
+        String[] projection = { MediaStore.Images.ImageColumns.DISPLAY_NAME };
+
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DISPLAY_NAME);
+
+        cursor.moveToFirst();
+
+        return cursor.getString(column_index);
+
+    }
+
+
+
+    // uri 아이디 찾기
+
+    private String getUriId(Uri uri) {
+
+        String[] projection = { MediaStore.Images.ImageColumns._ID };
+
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns._ID);
+
+        cursor.moveToFirst();
+
+        return cursor.getString(column_index);
+
     }
 
 
