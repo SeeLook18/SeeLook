b08a2a04d980728ec84a24c73e52fba1581ca9d0
daeun <sung980524@naver.com>
2020-08-18 17:08:29 +0900

    서씨가 햇대요

--- app/src/main/java/com/example/seelook/Post_Activity.java
+++ app/src/main/java/com/example/seelook/Post_Activity.java
@@ -1,30 +1,56 @@
 package com.example.seelook;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Post_Activity extends AppCompatActivity {
 
     private final int SELECT_IMAGE = 1;
     private final int SELECT_MOVIE = 2;
+    private StorageReference mStorageRef;
+    private VideoView videoView;
+    private Button add_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        videoView = (VideoView)findViewById(R.id.videoView);
+        videoView.setVisibility(View.INVISIBLE);
+
 
         Button home_btn = (Button)findViewById(R.id.home_btn);
         home_btn.setOnClickListener(new View.OnClickListener() {
@@ -48,72 +74,63 @@ public class Post_Activity extends AppCompatActivity {
             }
         });
 
-        Button add_btn = (Button)findViewById(R.id.add_button);
+        add_btn = (Button)findViewById(R.id.add_button);
         add_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-
                 i.setType("video/*");
-
                 i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
                 try {
-
                     startActivityForResult(i, SELECT_MOVIE);
 
                 } catch (android.content.ActivityNotFoundException e) {
-
                     e.printStackTrace();
-
                 }
             }
 
         });
+
+
     }
 
     @Override
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-
         super.onActivityResult(requestCode, resultCode, intent);
 
 
-
         if (resultCode == RESULT_OK) {
-
             if (requestCode == SELECT_IMAGE) {
-
                 Uri uri = intent.getData();
-
                 String path = getPath(uri);
-
                 String name = getName(uri);
-
                 String uriId = getUriId(uri);
 
                 Log.e("###",
-
                         "실제경로 : " + path + "\n파일명 : " + name + "\nuri : " + uri.toString() + "\nuri id : " + uriId);
-
             } else if (requestCode == SELECT_MOVIE) {
-
                 Uri uri = intent.getData();
-
                 String path = getPath(uri);
-
                 String name = getName(uri);
-
                 String uriId = getUriId(uri);
 
                 Log.e("###",
-
                         "실제경로 : " + path + "\n파일명 : " + name + "\nuri : " + uri.toString() + "\nuri id : " + uriId);
+                videoView.setVisibility(View.VISIBLE);
+                String videoFile = "//";
+                Bitmap thumbnail = ThumbnailUtils.createVideoThumbnail(videoFile,
+                        MediaStore.Images.Thumbnails.MINI_KIND);
+
+                BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(),thumbnail);
+                videoView.setBackground(bitmapDrawable);
+                videoView.setVideoURI(uri);
+                add_btn.setVisibility(View.INVISIBLE);
+                uploadFile(uri);
 
             }
-
         }
-
     }
 
     // 실제 경로 찾기
@@ -199,6 +216,56 @@ public class Post_Activity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
         startActivity(intent);
+    }
+
+    private void uploadFile(Uri uri) {
+        //업로드할 파일이 있으면 수행
+        if (uri != null) {
+            //업로드 진행 Dialog 보이기
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("업로드중...");
+            progressDialog.show();
+
+            //storage
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+
+            //Unique한 파일명을 만들자.
+            SimpleDateFormat formatter = new SimpleDateFormat("영상업로");
+            Date now = new Date();
+            String filename = formatter.format(now) + ".mp4";
+            //storage 주소와 폴더 파일명을 지정해 준다.
+            StorageReference storageRef = storage.getReferenceFromUrl("https://seelook-d3a93.firebaseio.com/users/-MEg1xwV7HEDW8OxSVgC").child("images/" + filename);
+            //올라가거라...
+            storageRef.putFile(uri)
+                    //성공시
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss(); //업로드 진행 Dialog 상자 닫기
+                            Toast.makeText(getApplicationContext(), "업로드 완료!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    //실패시
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "업로드 실패!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    //진행중
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            @SuppressWarnings("VisibleForTests") //이걸 넣어 줘야 아랫줄에 에러가 사라진다. 넌 누구냐?
+                                    double progress = (100 * taskSnapshot.getBytesTransferred()) /  taskSnapshot.getTotalByteCount();
+                            //dialog에 진행률을 퍼센트로 출력해 준다
+                            progressDialog.setMessage("Uploaded " + ((int) progress) + "% ...");
+                        }
+                    });
+        } else {
+            Toast.makeText(getApplicationContext(), "파일을 먼저 선택하세요.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
 
