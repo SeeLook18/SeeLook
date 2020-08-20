4b4195c72bafaee285e7d5490b2e33939692c9d7
haneulhaneul <tmajvm7960@naver.com>
2020-08-21 01:13:36 +0900

    파이어베이스 업로드 드디어..

--- app/src/main/java/com/example/seelook/Post_Activity.java
+++ app/src/main/java/com/example/seelook/Post_Activity.java
@@ -3,16 +3,13 @@ package com.example.seelook;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
@@ -22,38 +19,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnPausedListener;
-import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
-import java.net.URL;
 
 public class Post_Activity extends AppCompatActivity {
 
-    private final int SELECT_IMAGE = 1;
     private final int SELECT_MOVIE = 2;
-    private StorageReference storageRef;
+
     private VideoView videoView;
-    private Button add_btn;
+    private Button add_btn; //영상 가져오기
+    //private TextView post_title; //제목을 입력하세요
+    //private TextView post_content; //설명을 입력하세요
+
+    private StorageReference storageRef;
     private UploadTask uploadTask;
+
+
+    //하단 바 메뉴
+    private Button home_btn;
+    private Button profile_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_);
 
-        storageRef = FirebaseStorage.getInstance().getReference();
         videoView = (VideoView)findViewById(R.id.videoView);
         videoView.setVisibility(View.INVISIBLE);
 
+        MediaController mc = new MediaController(this);
+        videoView.setMediaController(mc); // Video View 에 사용할 컨트롤러 지정
 
-        Button home_btn = (Button)findViewById(R.id.home_btn);
+        storageRef = FirebaseStorage.getInstance().getReference();
+
+        home_btn = (Button)findViewById(R.id.home_btn);
         home_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,8 +67,8 @@ public class Post_Activity extends AppCompatActivity {
             }
         });
 
-        Button face_btn = (Button)findViewById(R.id.profile_btn);
-        face_btn.setOnClickListener(new View.OnClickListener() {
+        profile_btn = (Button)findViewById(R.id.profile_btn);
+        profile_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(
@@ -84,7 +87,7 @@ public class Post_Activity extends AppCompatActivity {
                 i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
                 try {
-                    startActivityForResult(i, SELECT_MOVIE);
+                    startActivityForResult(i,SELECT_MOVIE);
 
                 } catch (android.content.ActivityNotFoundException e) {
                     e.printStackTrace();
@@ -93,6 +96,8 @@ public class Post_Activity extends AppCompatActivity {
 
         });
 
+        //post_title = (TextView)findViewById(R.id.editTextTitle);
+        //post_content = (TextView)findViewById(R.id.editText);
 
     }
 
@@ -101,30 +106,14 @@ public class Post_Activity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
 
-
         if (resultCode == RESULT_OK) {
-            if (requestCode == SELECT_IMAGE) {
+            if (requestCode == SELECT_MOVIE) {
                 Uri uri = intent.getData();
                 String path = getPath(uri);
-                String name = getName(uri);
-                String uriId = getUriId(uri);
-
-
-                Log.e("###",
-                        "실제경로 : " + path + "\n파일명 : " + name + "\nuri : " + uri.toString() + "\nuri id : " + uriId);
-            } else if (requestCode == SELECT_MOVIE) {
-                Uri uri = intent.getData();
-                String path = getPath(uri);
-                String name = getName(uri);
-                String uriId = getUriId(uri);
-
-                Log.e("###",
-                        "실제경로 : " + path + "\n파일명 : " + name + "\nuri : " + uri.toString() + "\nuri id : " + uriId);
 
                 //firebase 업로드
-
                 Uri file = Uri.fromFile(new File(path));
-                StorageReference mStorageRef = storageRef.child("videos/"+file.getLastPathSegment());
+                StorageReference mStorageRef = storageRef.child("sample/"+file.getLastPathSegment());
                 uploadTask = mStorageRef.putFile(file);
 
                 // Register observers to listen for when the download is done or if it fails
@@ -132,81 +121,35 @@ public class Post_Activity extends AppCompatActivity {
                     @Override
                     public void onFailure(@NonNull Exception exception) {
                         // Handle unsuccessful uploads
+                        //Toast.makeText(Post_Activity.this, "이미지 업로드 실패", Toast.LENGTH_LONG).show();
                     }
                 }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                         // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
                         // ...
+                        //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        //Toast.makeText(Post_Activity.this, "이미지 업로드 성공", Toast.LENGTH_LONG).show();
                     }
                 });
 
-
-                //썸네일 띄우기
                 videoView.setVisibility(View.VISIBLE);
-                String videoFile = "//";
-                Bitmap thumbnail = ThumbnailUtils.createVideoThumbnail(videoFile,
-                        MediaStore.Images.Thumbnails.MINI_KIND);
-
-                BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(),thumbnail);
-                videoView.setBackground(bitmapDrawable);
                 videoView.setVideoURI(uri);
                 add_btn.setVisibility(View.INVISIBLE);
 
+                videoView.requestFocus(); // 포커스 얻어오기
+                videoView.start();
             }
         }
     }
 
     // 실제 경로 찾기
-
     private String getPath(Uri uri) {
-
         String[] projection = { MediaStore.Images.Media.DATA };
-
         Cursor cursor = managedQuery(uri, projection, null, null, null);
-
         int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-
         cursor.moveToFirst();
-
         return cursor.getString(column_index);
-
-    }
-
-
-
-    // 파일명 찾기
-
-    private String getName(Uri uri) {
-
-        String[] projection = { MediaStore.Images.ImageColumns.DISPLAY_NAME };
-
-        Cursor cursor = managedQuery(uri, projection, null, null, null);
-
-        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DISPLAY_NAME);
-
-        cursor.moveToFirst();
-
-        return cursor.getString(column_index);
-
-    }
-
-
-
-    // uri 아이디 찾기
-
-    private String getUriId(Uri uri) {
-
-        String[] projection = { MediaStore.Images.ImageColumns._ID };
-
-        Cursor cursor = managedQuery(uri, projection, null, null, null);
-
-        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns._ID);
-
-        cursor.moveToFirst();
-
-        return cursor.getString(column_index);
-
     }
 
 
@@ -217,7 +160,7 @@ public class Post_Activity extends AppCompatActivity {
         builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(getApplicationContext(), "확인버튼이 눌렸습니다", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "게시물 작성 완료", Toast.LENGTH_LONG).show();
             }
         });
         builder.setNegativeButton("취소", null).show();
@@ -225,22 +168,6 @@ public class Post_Activity extends AppCompatActivity {
         alertDialog.show();
     }
 
-    public void upload_clicked(View v){
-        DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                doTakeAlbumAction();
-            }
-        };
-    }
-
-    public void doTakeAlbumAction() // 앨범에서 이미지 가져오기
-    {
-        // 앨범 호출
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
-        startActivity(intent);
-    }
 }
 
 
