610dc42d154bde2fd0ca35697ec7976da1d18ad0
daeun <sung980524@naver.com>
2020-08-24 12:22:54 +0900

    인증 3분 타이머

--- app/src/main/java/com/example/seelook/Auth_Email_Activity.java
+++ app/src/main/java/com/example/seelook/Auth_Email_Activity.java
@@ -2,11 +2,23 @@ package com.example.seelook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Auth_Email_Activity extends AppCompatActivity {
+public class Auth_Email_Activity extends AppCompatActivity{
+
+    TextView time_counter;
+    Button auth_Button;//인증 번호 확인 버튼
+    CountDownTimer countDownTimer;
+
+    final int MILLISINFUTURE=180*1000;
+    final int COUNT_DOWN_INTERVAL=1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +31,27 @@ public class Auth_Email_Activity extends AppCompatActivity {
                 finish();
             }
         });
+
+        countDownTimer();
+    }
+
+    public void countDownTimer(){
+        time_counter = (TextView) findViewById(R.id.time_counter);
+        auth_Button=(Button)findViewById(R.id.check);//확인 버튼
+
+        countDownTimer=new CountDownTimer(MILLISINFUTURE,COUNT_DOWN_INTERVAL){
+            public void onTick(long millisUntilFinished){
+                long emailAuthCount=millisUntilFinished/1000;
+
+                if ((emailAuthCount - ((emailAuthCount / 60) * 60)) >= 10) { //초가 10보다 크면 그냥 출력
+                    time_counter.setText((emailAuthCount / 60) + " : " + (emailAuthCount - ((emailAuthCount / 60) * 60)));
+                } else { //초가 10보다 작으면 앞에 '0' 붙여서 같이 출력. ex) 02,03,04...
+                    time_counter.setText((emailAuthCount / 60) + " : 0" + (emailAuthCount - ((emailAuthCount / 60) * 60)));
+                }
+            }
+            public void onFinish(){
+                time_counter.setText("00:00");
+            }
+        }.start();
     }
 }
