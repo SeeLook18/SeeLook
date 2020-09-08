416ed27601597bd1e68415d3c26326064efde66a
minju9809 <minju9809@naver.com>
2020-09-08 22:25:07 +0900

    권한 허용 메시지창 띄우기

--- app/src/main/java/com/example/seelook/Home_Activity1.java
+++ app/src/main/java/com/example/seelook/Home_Activity1.java
@@ -1,13 +1,19 @@
 package com.example.seelook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -22,6 +28,8 @@ public class Home_Activity1 extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_1);
+
+        checkSelfPermission();//권한 허용 팝업
 
         Button btn1 = (Button)findViewById(R.id.home_btn); //새로고침 기능 넣어주기
         /*btn1.setOnClickListener(new View.OnClickListener() {
@@ -90,4 +98,45 @@ public class Home_Activity1 extends AppCompatActivity{
             }
         });
     }
+
+    //권한에 대한 응답이 있을때 작동하는 함수
+    @Override public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //권한을 허용 했을 경우
+        if(requestCode == 1){
+            int length = permissions.length;
+            for (int i = 0; i < length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    // 동의
+                    Log.d("MainActivity","권한 허용 : " + permissions[i]);
+                }
+            }
+        }
+    }
+
+
+    public void checkSelfPermission() { String temp = ""; //파일 읽기 권한 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            temp += Manifest.permission.READ_EXTERNAL_STORAGE + " ";
+        } //파일 쓰기 권한 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " ";
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.CAMERA+ " ";
+        }//카메라 권한
+        if (TextUtils.isEmpty(temp) == false)
+        { // 권한 요청
+            ActivityCompat.requestPermissions(this, temp.trim().split(" "),1);
+        }
+        else
+        {
+            // 모두 허용 상태
+            Toast.makeText(this, "권한을 모두 허용", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
